f0dee68f824bafb1b2e1e7283f01baf765463e86
Matteo Collina <matteo.collina@gmail.com>
2014-05-07 10:25:35 +0200

    Added act support.

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 var entityStream  = require('seneca-entity-save-stream')
+  , actStream     = require('seneca-act-stream')
   , csvStream     = require('csv2')
   , headStream    = require('head-stream')
   , through       = require('through2')
 
-function importCSV(seneca, entityName$) {
+function importCSV(seneca, opts) {
 
   var csv   = csvStream()
     , head  = headStream(firstRow)
+    , dest  = buildDestStream(seneca, opts)
 
   function firstRow(header, done) {
-
-    var entity = entityStream(seneca, { name$: entityName$ })
 
     function rowToObject(row) {
       var obj = {}
@@ -27,9 +27,9 @@ function importCSV(seneca, entityName$) {
         this.push(rowToObject(chunk))
         callback()
       }))
-      .pipe(entity)
+      .pipe(dest)
 
-    entity.on('finish', function() {
+    dest.on('finish', function() {
       csv.emit('importCompleted')
     })
 
@@ -39,6 +39,16 @@ function importCSV(seneca, entityName$) {
   csv.pipe(head)
 
   return csv
+}
+
+function buildDestStream(seneca, opts) {
+  if (opts.entity) {
+    return entityStream(seneca, { name$: opts.entity })
+  } else if (opts.act) {
+    return actStream(seneca, opts.act)
+  } else {
+    throw new Error('unknown opts')
+  }
 }
 
 module.exports = importCSV
--- test.js
+++ test.js
@@ -6,21 +6,38 @@ var test      = require('tap').test
 test('importing csv lines as entities', function(t) {
   var s         = seneca()
     , pear      = s.make('pear')
-    , instance  = importer(s, 'pear')
+    , instance  = importer(s, { entity: 'pear' })
 
   instance.end('name,price\nhello,200\n')
 
   instance.on('importCompleted', function() {
     pear.list$({}, function(err, res) {
-      t.notOk(err)
-      t.equal(res.length, 1)
-
-      if (res[0]) {
-        t.equal(res[0].name, 'hello')
-        t.equal(res[0].price, '200')
-      }
+      t.notOk(err, 'no error')
+      t.equal(res.length, 1, 'one result')
+      t.equal(res[0].name, 'hello', 'same name')
+      t.equal(res[0].price, '200', 'same price')
 
       t.end()
     })
   })
+})
+
+test('acting on every csv line', function(t) {
+  t.plan(1)
+
+  var s         = seneca()
+    , pear      = s.make('pear')
+    , pattern   = { sample: 'call' }
+    , instance  = importer(s, { act: pattern })
+    , message   = { sample: 'call', name: 'hello', price: '200' }
+
+  function check(arrived, done) {
+    delete arrived.actid$
+    t.deepEqual(arrived, message)
+    done(null)
+  }
+
+  s.add(pattern, check);
+
+  instance.end('name,price\nhello,200\n')
 })
